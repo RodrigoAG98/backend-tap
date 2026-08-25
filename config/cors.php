97e098108c5ac0243b9aa6847a358a15f59463d1
RodrigoAG98 <rodrigo-dea@hotmail.com>
2026-08-24 22:06:46 -0600

    Fix: se agrega url exacta

--- config/cors.php
+++ config/cors.php
@@ -22,7 +22,7 @@ return [
     'allowed_origins' => [
         'http://localhost:4200',
         'http://127.0.0.1:4200',
-        'https://*.vercel.app',
+        'https://frontend-tap-delta.vercel.app/',
     ],
 
     'allowed_origins_patterns' => [],
