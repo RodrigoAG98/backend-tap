0e2ac30caeda4887658089bfe2e323f70761846b
RodrigoAG98 <rodrigo-dea@hotmail.com>
2026-08-24 19:50:17 -0600

    dockerfile sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Asegurar que Apache (www-data) sea dueño de las carpetas de almacenamiento y logs
+chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
+
 # Esperar unos segundos o ejecutar comandos de inicialización
 echo "Ejecutando optimizaciones de Laravel..."
 php artisan config:clear
